f0751ae71d47c8de63c5b9b3e3ffb0bb9de5b9d0
gleb <gleby-hleby@inbox.ru>
2019-10-26 12:56:42 +0300

    Scripted auto-commit on change (2019-10-26 12:56:42) by gitwatch.sh

--- src/main/java/Time.java
+++ src/main/java/Time.java
@@ -10,13 +10,14 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job; 
 import org.apache.hadoop.mapreduce.Mapper; 
 import org.apache.hadoop.mapreduce.Reducer; 
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat; 
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat; 
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class Time {
     public static void main(String[] args) throws Exception { 
         Job job = Job.getInstance();
 
-
+        MultipleInputs.addInputPath(job, );
     } 
 }

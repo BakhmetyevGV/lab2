ccce55cc2693ccadf9b9af0cb93ee4af3fc67c2a
gleb <gleby-hleby@inbox.ru>
2019-10-30 23:32:27 +0300

    Scripted auto-commit on change (2019-10-30 23:32:27) by gitwatch.sh

--- src/main/java/GroupingComparator.java
+++ src/main/java/GroupingComparator.java
@@ -1,7 +1,12 @@
 import org.apache.hadoop.io.WritableComparator;
 
 public class GroupingComparator extends WritableComparator {
+    public GroupingComparator(){
+        super(GroupingComparator.class, true);
+    }
 
     @Override
-    public int compare()
+    public int compare(){
+
+    }
 }

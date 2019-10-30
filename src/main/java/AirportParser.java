4dda868aaf7d4768fc07fbba3fe406663e8daaca
gleb <gleby-hleby@inbox.ru>
2019-10-30 22:51:56 +0300

    Scripted auto-commit on change (2019-10-30 22:51:56) by gitwatch.sh

--- src/main/java/AirportParser.java
+++ src/main/java/AirportParser.java
@@ -1,10 +1,14 @@
-
+import java.util.Arrays;
 
 public class AirportParser  {
-    public String[] parse(String data, String delimeter){
+    private String[] EMPTY = {};
+    public String[] split(String data, String delimeter){
         String[] columns = {};
-        columns = data.split(delimeter);
 
+        columns = data.split(delimeter, 2);
+
+        if(columns[0] == "Code")
+            return EMPTY;
 
         return columns;
     }

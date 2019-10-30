d4cac5aa1c03b81cc9b156e1afcdcef95ad7f3e9
gleb <gleby-hleby@inbox.ru>
2019-10-30 23:45:01 +0300

    Scripted auto-commit on change (2019-10-30 23:45:01) by gitwatch.sh

--- src/main/java/Reducer.java
+++ src/main/java/Reducer.java
@@ -1,17 +1,31 @@
 import org.w3c.dom.Text;
 
 import java.io.IOException;
+import java.util.Iterator;
+
+import static java.sql.Types.DOUBLE;
 
 public class Reducer extends org.apache.hadoop.mapreduce.Reducer<AirportWritableComparable, Text, Text, Text> {
-    @Override
-    protected void reduce(AirportWritableComparable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-        super.reduce(key, values, context);
-    }
 
     @Override
-    public static void reduce(){
+    protected void reduce(AirportWritableComparable key, Iterable<Text> values, Context context)
+            throws IOException, InterruptedException {
+        Iterator<Text> it = values.iterator();
 
+        double min = Double.MAX_VALUE;
+        double max = Double.MIN_VALUE;
+        double sum = 0;
+        int num = 0;
+
+        String name = "";
+
+        while (it.hasNext()){
+            String s = it.next().toString();
+            if(s.equals("")){
+                num++;
+                continue;
+            }
+            if(s.)
+        }
     }
-
-
 }

b013ece350449ce34b47a1d26e96881db6cc4ce9
gleb <gleby-hleby@inbox.ru>
2019-10-30 23:22:02 +0300

    Scripted auto-commit on change (2019-10-30 23:22:02) by gitwatch.sh

--- src/main/java/AirportMapper.java
+++ src/main/java/AirportMapper.java
@@ -5,12 +5,16 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class AirportMapper extends Mapper<LongWritable, Text, AirportWritableComparable, Text> {
+    public int AIRPORT_ID = 0;
+    public int AIRPORT_NAME = 1;
+    public int KEY = 0;
 
     @Override
-    protected void map(LongWritable a, Text data, Context context) throws IOException {
+    protected void map(LongWritable a, Text data, Context context) throws IOException, InterruptedException {
         String[] parsed = AirportParser.parse(data.toString(),",");
 
-        context.write()
+        context.write(new AirportWritableComparable(Integer.parseInt(parsed[AIRPORT_ID]), KEY),
+                new Text(parsed[AIRPORT_NAME]));
     }
 
 }
